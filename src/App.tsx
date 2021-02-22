bcd43e5aac0fc67147303185386956ee31ab256f
Raphael <raphael@agecomnet.com.br>
2021-02-22 19:35:23 -0300

    dia 01

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../src/styles/global.css";
+import { ExpirienceBar } from "./components/ExepirienceBar";
+function App() {
+  return (
+    <div className="conteiner">
+      <ExpirienceBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExepirienceBar.tsx
@@ -0,0 +1,14 @@
+export function ExpirienceBar(){
+    return(
+        <header className="expirienceBar">
+            <span>0 xp</span>
+            <div>
+                < div style={{width:"50%"}} />
+                <span className="current-experience" style={{left:"50%"}}>
+                300 xp
+                </span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    );
+}
